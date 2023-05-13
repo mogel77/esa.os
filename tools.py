9f0d3d8f1cc62c1bb8db76d0b484254368241f76
mogel <112684002+mogel77@users.noreply.github.com>
2023-05-13 20:49:22 +0200

    CHG: capitalize

--- tools.py
+++ tools.py
@@ -13,6 +13,6 @@ def getDictItem(dict, key_normal, key_localized="foo"):
     # liefert das Item, schaut zuerst nach dem
     # für die aktuelle Sprache, wenn nicht dann der English/Game Name
     # oder None - wenn nix vorhanden
-    if key_localized in dict: return dict[key_localized]
-    if key_normal in dict: return dict[key_normal]
+    if key_localized in dict: return dict[key_localized].capitalize()
+    if key_normal in dict: return dict[key_normal].capitalize()
     return "unnamed"
